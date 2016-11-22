db15a2bae3f92a08eb24374bdffd37d9493d10d9
Ciarah Desiree Suligan <Ciarah Desiree Suligan>
2016-11-22 21:21:38 +1300

    Clear and Currency Rates working

--- Contoso Bank/Contoso Bank/Controllers/MessagesController.cs
+++ Contoso Bank/Contoso Bank/Controllers/MessagesController.cs
@@ -22,115 +22,59 @@ namespace Contoso_Bank
         /// </summary>
         public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
         {
-            // Global values
-            bool boolAskedForChoice = false;
-            //string strUserName = "";
-            string userChoice = "";
+
             if (activity.Type == ActivityTypes.Message)
             {
-                // Get any saved values
-                StateClient sc = activity.GetStateClient();
-                BotData userData = sc.BotState.GetPrivateConversationData(activity.ChannelId, activity.Conversation.Id, activity.From.Id);
-                boolAskedForChoice = userData.GetProperty<bool>("AskedForChoice");
-                userChoice = userData.GetProperty<string>("Choice") ?? "";
-                // Create text for a reply message   
-                StringBuilder strReplyMessage = new StringBuilder();
-                if (boolAskedForChoice == false) // Never asked for choice
-                {
-                    strReplyMessage.Append($"Welcome! I am Contoso Bank's Bot.");
-                    strReplyMessage.Append($"\n");
-                    strReplyMessage.Append($"Would you like to use 'Currency Rates' or 'Stocks'?");
-                    // Set BotUserData
-                    userData.SetProperty<bool>("AskedForChoice", true);
-                }
-                else // Have asked for choice
-                {
-                    if (userChoice == "") // Choice was never provided
-                    {
-                        userChoice = activity.Text; //Either Currency rates of Stocks
-                        if (userChoice.ToLower() == "currency exchange") {
-                            strReplyMessage.Append($"Your choice was: {userChoice}");
-                        }
-
-                        else if (userChoice.ToLower() == "stocks")
-                        {
-                            strReplyMessage.Append($"Your choice was: {userChoice}");
-                        }
-
-                    }
-                    else // Choice was provided
-                    {
-                        strReplyMessage.Append($"You've been provided a choice before this.");
-                    }
-                }
-                // Save BotUserData
-                sc.BotState.SetPrivateConversationData(activity.ChannelId, activity.Conversation.Id, activity.From.Id, userData);
-                // Create a reply message
                 ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
-                Activity replyMessage = activity.CreateReply(strReplyMessage.ToString());
-                await connector.Conversations.ReplyToActivityAsync(replyMessage);
-         
+
+                StateClient stateClient = activity.GetStateClient(); //Saves bot state
+                BotData userData = await stateClient.BotState.GetUserDataAsync(activity.ChannelId, activity.From.Id); //Invoking state client
+
+                var userMessage = activity.Text; //The user's input
+
+                Dictionary<string, int> currencyCodes = new Dictionary<string, int>()
+                        { { "AUD", 1}, { "BGN", 1}, { "BRL", 3}, { "CAD", 1}, { "CHF", 1}, { "CNY", 6}, { "CZK", 2}, { "DKK", 6}, { "GBP", 0},
+                            { "HKD", 7}, { "HRK", 7}, { "HUF", 2}, { "IDR", 0}, { "ILS", 3}, { "INR", 0}, { "JPY", 1}, { "KRW", 1}, { "MXN", 2},
+                            { "MYR", 4}, { "NOK", 8}, { "NZD", 1}, { "PHP", 4}, { "PLN", 4}, { "RON", 4}, { "RUB", 6}, { "SEK", 9}, { "SGD", 1},
+                            { "THB", 3}, { "TRY", 3}, { "ZAR", 1}, { "EUR", 0} };
 
 
-                /*ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
-                CurrencyExchange.RootObject rootObject;
-                HttpClient client = new HttpClient();
+                string endOutput = "Welcome to Contoso Bank! Would you like currency rates or stocks?";
 
+                // calculate something for us to return
+                if (userData.GetProperty<bool>("SentGreeting"))
+                {
+                    endOutput = "Hi again! Welcome to Contoso Bank's currency exchange rates! Would you like currency rates or stocks?";
+                }
 
-                Activity reply = activity.CreateReply($"Welcome to Contoso Bank's Currency Converter. What is your intended base currency?");
-                await connector.Conversations.ReplyToActivityAsync(reply);
+                else
+                {
+                    userData.SetProperty("SentGreeting", true);
+                    await stateClient.BotState.SetUserDataAsync(activity.ChannelId, activity.From.Id, userData);
+                    //^Syncing updated userData
+                    //If we didn't sync up, the changes will not be reflected if we asked for the user data again. 
+                    //In our first greeting, the Bot will say "Hello".
+                    //When we greet the Bot again, it will say "Hello again.".
+                }
 
-                string baseCurrency = activity.Text;
-                string newCurrency;
+                if (userMessage.ToLower().Contains("currency rates"))
+                { //The user wants Currency Exchange
+                    userData.SetProperty("UserWantsCurrencyRates", true); //Sets "userWantsCurrencyRates" to TRUE
+                    endOutput = "userMessage is: " + userMessage;
+                }
 
-                string x = await client.GetStringAsync(new Uri("http://api.fixer.io/latest?base=" + baseCurrency));
-
-                rootObject = JsonConvert.DeserializeObject<CurrencyExchange.RootObject>(x);
-
-                if (rootObject.error.Length <= 0) {
-                    Activity reply2 = activity.CreateReply($"What is the currency you would like to convert into?");
-                    await connector.Conversations.ReplyToActivityAsync(reply2);
-                    newCurrency = activity.Text;
-
-                    if (newCurrency.ToUpper() == "GBP")
-                    {
-                        rootObject = JsonConvert.DeserializeObject<CurrencyExchange.RootObject>(x);
-                        string result = rootObject.rates.GBP + "";
-
-                        //Activity reply3 = activity.CreateReply($"From: " + baseCurrency + " to " + newCurrency + " is: " +result );
-                        //await connector.Conversations.ReplyToActivityAsync(reply3);
-                    }
-                } */
-
-
-
-                /*StringBuilder strReplyMessage = new StringBuilder();
-                strReplyMessage.Append($"Welcome to Contoso Bank's currency converter.");
-                Activity replyMessage = activity.CreateReply(strReplyMessage.ToString());
-                await connector.Conversations.ReplyToActivityAsync(replyMessage);
-
-                Activity reply1 = activity.CreateReply($"Please state Base Currency.");
-                await connector.Conversations.ReplyToActivityAsync(reply1);
-                string baseCurrency = activity.Text;
-
-                Activity reply2 = activity.CreateReply($"Please state New Currency.");
-                await connector.Conversations.ReplyToActivityAsync(reply2);
-                string newCurrency = activity.Text;
-
-                Activity reply3 = activity.CreateReply($"Please state the amount you would like to convert.");
-                await connector.Conversations.ReplyToActivityAsync(reply3);
-                string amountToConvert = activity.Text;*/
-
-                //string x = await client.GetStringAsync(new Uri("http://apilayer.net/api/convert?access_key=c5cc7aed368f5eda94741002077023e3&from=" + baseCurrency + "&to=" + newCurrency + "&amount=" + amountToConvert + ""));
+                if (userMessage.ToLower().Contains("clear"))
+                {
+                    endOutput = "Your personal data has been cleared.";
+                    await stateClient.BotState.DeleteStateForUserAsync(activity.ChannelId, activity.From.Id);
+                }
 
 
 
 
-                /*Dictionary<String, int> currenciesDict = { "AUD": 1, "BGN": rootObject.rates.AUD, "BRL": rootObject.rates.AUD, "CAD": rootObject.rates.AUD, "CHF": rootObject.rates.AUD, "CNY": rootObject.rates.AUD, "CZK": rootObject.rates.AUD, "DKK": rootObject.rates.AUD,  
-                    "GBP": rootObject.rates.AUD, "HKD": rootObject.rates.AUD, "HRK": rootObject.rates.AUD, "HUF": rootObject.rates.AUD, "IDR": rootObject.rates.AUD, "ILS": rootObject.rates.AUD, "INR": rootObject.rates.AUD,  "JPY": rootObject.rates.AUD,
-                    "KRW": rootObject.rates.AUD, "MXN": rootObject.rates.AUD, "MYR": rootObject.rates.AUD, "NOK": rootObject.rates.AUD, "NZD": rootObject.rates.AUD, "PHP": rootObject.rates.AUD, "PLN": rootObject.rates.AUD, "RON": rootObject.rates.AUD, 
-                    "RUB": rootObject.rates.AUD, "SEK": rootObject.rates.AUD, "SGD": rootObject.rates.AUD, "THB": rootObject.rates.AUD, "TRY": rootObject.rates.AUD, "USD": rootObject.rates.AUD, "ZAR": rootObject.rates.AUD, "EUR": rootObject.rates.AUD;
-                        };*/
+                // return our reply to the user
+                Activity infoReply = activity.CreateReply(endOutput);
+                await connector.Conversations.ReplyToActivityAsync(infoReply);
 
             }
             else
@@ -141,24 +85,18 @@ namespace Contoso_Bank
             return response;
         }
 
+
+
+
+
+
+
+
         private Activity HandleSystemMessage(Activity message)
         {
             if (message.Type == ActivityTypes.DeleteUserData)
             {
-                // Get BotUserData
-                StateClient sc = message.GetStateClient();
-                BotData userData = sc.BotState.GetPrivateConversationData(
-                    message.ChannelId, message.Conversation.Id, message.From.Id);
-                // Set BotUserData
-                userData.SetProperty<string>("Choice", "");
-                userData.SetProperty<bool>("AskedForChoice", false);
-                // Save BotUserData
-                sc.BotState.SetPrivateConversationData(
-                    message.ChannelId, message.Conversation.Id, message.From.Id, userData);
-                // Create a reply message
-                ConnectorClient connector = new ConnectorClient(new Uri(message.ServiceUrl));
-                Activity replyMessage = message.CreateReply("Personal data has been deleted.");
-                return replyMessage;
+
             }
             else if (message.Type == ActivityTypes.ConversationUpdate)
             {
