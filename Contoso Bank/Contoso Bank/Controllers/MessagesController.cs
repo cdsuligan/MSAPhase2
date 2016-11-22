9cf56e9f4060aceb5885ee5c4fd3433888fe85d1
Ciarah Desiree Suligan <Ciarah Desiree Suligan>
2016-11-23 00:14:25 +1300

    Conversion is now working.

--- Contoso Bank/Contoso Bank/Controllers/MessagesController.cs
+++ Contoso Bank/Contoso Bank/Controllers/MessagesController.cs
@@ -36,7 +36,7 @@ namespace Contoso_Bank
                         { { "AUD", 1}, { "BGN", 1}, { "BRL", 3}, { "CAD", 1}, { "CHF", 1}, { "CNY", 6}, { "CZK", 2}, { "DKK", 6}, { "GBP", 0},
                             { "HKD", 7}, { "HRK", 7}, { "HUF", 2}, { "IDR", 0}, { "ILS", 3}, { "INR", 0}, { "JPY", 1}, { "KRW", 1}, { "MXN", 2},
                             { "MYR", 4}, { "NOK", 8}, { "NZD", 1}, { "PHP", 4}, { "PLN", 4}, { "RON", 4}, { "RUB", 6}, { "SEK", 9}, { "SGD", 1},
-                            { "THB", 3}, { "TRY", 3}, { "ZAR", 1}, { "EUR", 0} };
+                            { "THB", 3}, { "TRY", 3}, { "ZAR", 1}, { "EUR", 0}, { "USD", 1} };
 
 
                 string endOutput = "Welcome to Contoso Bank! Would you like currency rates or stocks?";
@@ -57,22 +57,92 @@ namespace Contoso_Bank
                     //When we greet the Bot again, it will say "Hello again.".
                 }
 
-                if (userMessage.ToLower().Contains("currency rates"))
-                { //The user wants Currency Exchange
-                    userData.SetProperty("UserWantsCurrencyRates", true); //Sets "userWantsCurrencyRates" to TRUE
-                    endOutput = "userMessage is: " + userMessage;
-                }
-
                 if (userMessage.ToLower().Contains("clear"))
                 {
                     endOutput = "Your personal data has been cleared.";
                     await stateClient.BotState.DeleteStateForUserAsync(activity.ChannelId, activity.From.Id);
                 }
 
+                if (userMessage.ToLower().Contains("currency rates"))
+                { //The user wants Currency Exchange
+                    endOutput = "Currency Rates is: " + userMessage + ". What is your base currency code?";
+                    userData.SetProperty("UserWantsCurrencyRates", true);//Sets "UserWantsCurrencyRates" to TRUE
+                    await stateClient.BotState.SetUserDataAsync(activity.ChannelId, activity.From.Id, userData);
+
+                }
+
+
+                if (userData.GetProperty<bool>("UserWantsCurrencyRates") & (userMessage.Length == 3)) //Checks if the UserWantsCurrencyRates 
+                {
+                    if (currencyCodes.ContainsKey(userMessage.ToUpper())) //Checks if valid Currency Code
+                    {
+                        userData.SetProperty<string>("BaseCurrency", userMessage); //Setting the BaseCurrency to userMessage (the user's input)
+                        await stateClient.BotState.SetUserDataAsync(activity.ChannelId, activity.From.Id, userData);
+
+                        userData.SetProperty("UserWantsCurrencyRates", false);
+                        await stateClient.BotState.SetUserDataAsync(activity.ChannelId, activity.From.Id, userData);
+
+                        endOutput = "What is the new currency?";
+
+                    }
+                }
+
+                if (userData.GetProperty<string>("BaseCurrency") != null) //Checks if BaseCurrency exists
+                {
+                    if ((userMessage.Length == 3) & (userMessage != userData.GetProperty<string>("BaseCurrency") & (userData.GetProperty<string>("BaseCurrency").Length ==3)))
+                    {
+                        if (currencyCodes.ContainsKey(userMessage.ToUpper())) //Checks if valid Currency Code
+                        {
+                            string baseCurrency = userData.GetProperty<string>("BaseCurrency");
+                            string newCurrency = userMessage;
+                            double result = 0.0;
+
+                            CurrencyExchange.RootObject rootObject;
+                            HttpClient client = new HttpClient();
+
+                            string x = await client.GetStringAsync(new Uri("http://api.fixer.io/latest?base=" + baseCurrency));
+
+                            rootObject = JsonConvert.DeserializeObject<CurrencyExchange.RootObject>(x);
 
 
 
-                // return our reply to the user
+                            if (newCurrency.ToUpper() == "AUD")
+                            {
+                                result = rootObject.rates.AUD;
+                            }
+
+                            else if (newCurrency.ToUpper() == "BGN")
+                            {
+                                result = rootObject.rates.BGN;
+                            }
+
+                            else if (newCurrency.ToUpper() == "BRL")
+                            {
+                                result = rootObject.rates.BRL;
+                            }
+
+                            endOutput = "BaseCurrency is: " + baseCurrency + " newCurrency is: " + userMessage + " and the result is: " + result.ToString();
+
+                            userData.SetProperty<string>("BaseCurrency", "");
+                            await stateClient.BotState.SetUserDataAsync(activity.ChannelId, activity.From.Id, userData);
+
+                        }
+
+                        else
+                        {
+                            endOutput = "Invalid currency code. Please try again.";
+                        }
+                    }
+
+                }
+
+
+
+
+
+
+
+
                 Activity infoReply = activity.CreateReply(endOutput);
                 await connector.Conversations.ReplyToActivityAsync(infoReply);
 
